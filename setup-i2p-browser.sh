45e2e1ae58eb4bbd130b4c460dbddfc773451f8d
idk <problemsolver@openmailbox.org>
2018-05-23 15:17:57 -0400

    even more reduced changes to syspref.js, started script to modify existing(already installed) TBB in same way.

--- setup-i2p-browser.sh
+++ setup-i2p-browser.sh
@@ -3,9 +3,15 @@
 validate_i2pbrowser_directory(){
     temp="$1"
     if [ -d "$temp" ]; then
-        echo "$temp"
+        if [ -f "$temp/Browser/TorBrowser/Data/Browser/profile.default/preferences/extension-overrides.js" ]; then
+            echo "$temp"
+        else
+            echo "error; i2pbrowser_directory does not appear to be a TBB directory" 1>&2
+            exit 1
+        fi
     else
         echo "error; i2pbrowser_directory is not a directory" 1>&2
+        exit 1
     fi
 }
 
@@ -13,8 +19,9 @@ if [ ! -z "$1" ]; then
     i2pbrowser_directory=$(validate_i2pbrowser_directory "$1")
 fi
 
-if [ -z "$i2pbrowser_directory" ]
+if [ -z "$i2pbrowser_directory" ]; then
     echo "error; i2pbrowser_directory unset" 1>&2
     exit 1
 fi
 
+echo "$i2pbrowser_directory"
